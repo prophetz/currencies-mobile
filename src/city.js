cfdf245f41cbcdf9bbd677b9c280c24fc6f429ce
kpuzuc <kpuzuc@gmail.com>
2016-07-02 16:57:12 +0300

    fix

--- src/city.js
+++ src/city.js
@@ -1,22 +1,29 @@
 var registry = require("./registry");
 var language = require("./language");
+var currency = require("./currency");
 
 var City = function () {
 
     var self = this;
 
+    this.selectCityPage = null;
+    this.citiesList = null;
+
     this.load = function (callback) {
 
-        console.log(registry.get('domain'));
+        console.log('city.load');
 
-        fetch(registry.get('domain') + "/cities/get").then(function (response) {
+        fetch(registry.get('domain') + "/city/get").then(function (response) {
 
             var cities = JSON.parse(response._bodyInit);
 
             var items = [];
-            items.push({name: 'Выберите город', type: "section"});
             for (var i = 0; i < cities.length; i++) {
-                items.push({name: cities[i], type: "item"});
+                if (cities[i].type == 'region') {
+                    items.push({name: cities[i].name, type: "section"});
+                } else {
+                    items.push({name: cities[i].name, type: "item", alt_name: cities[i].altName, id: cities[i].id});
+                }
             }
 
             callback(items);
@@ -26,54 +33,70 @@ var City = function () {
         });
     };
 
-    this.checkSelectedCity = function () {
+    this.checkSelectedCity = function (callback) {
+
+        console.log('city.checkSelectedCity');
+
+        self.createSelectCityPage();
+
         var selectedCity = localStorage.getItem('city');
+
+        //selectedCity = null;
 
         if (selectedCity == null) {
 
+            self.selectCityPage.open();
+
             self.load(function (cities) {
-
-                var selectCityPage = tabris.create("Page", {
-                    title: language.getString('selectCityPage', 'title'),
-                    background: "#F2F2F2",
-                    topLevel: true
-                });
-
-                new tabris.TextView({
-                    layoutData: {left: 10, top: "prev() 10", right: 10},
-                    text: "Выберите, пожалуйста, ваш город:",
-                    alignment: "center"
-                }).appendTo(selectCityPage);
-
-                new tabris.CollectionView({
-                    layoutData: {left: 0, top: 0, right: 0, bottom: 0},
-                    items: cities,
-                    cellType: function (item) {
-                        return item.type;
-                    },
-                    itemHeight: function (item, type) {
-                        return type === "section" ? 48 : 48;
-                    },
-                    initializeCell: function (cell, type) {
-                        var textView = new tabris.TextView({
-                            layoutData: {top: 2, bottom: 2, left: 5, right: 5},
-                            font: type === "section" ? "bold 28px" : "28px",
-                            alignment: type === "section" ? "center" : "center"
-                        }).appendTo(cell);
-                        if (type === "section") {
-                            cell.set("background", "#cecece");
-                        }
-                        cell.on("change:item", function (widget, item) {
-                            textView.set("text", item.name);
-                        });
-                    }
-                }).appendTo(selectCityPage);
-
-                selectCityPage.open();
+                self.citiesList.insert(cities);
             });
+
+        } else {
+            callback();
         }
+    };
+
+    this.createSelectCityPage = function () {
+
+        self.selectCityPage = tabris.create("Page", {
+            title: language.getString('selectCityPage', 'title'),
+            background: "#F2F2F2",
+            topLevel: true
+        }).on("appear", function () {
+            self.load(function (cities) {
+                self.citiesList.insert(cities);
+            });
+        });
+
+        self.citiesList = new tabris.CollectionView({
+            layoutData: {left: 0, top: 0, right: 0, bottom: 0},
+            cellType: function (item) {
+                return item.type;
+            },
+            itemHeight: function (item, type) {
+                return type === "section" ? 48 : 48;
+            },
+            initializeCell: function (cell, type) {
+                var textView = new tabris.TextView({
+                    layoutData: {top: 2, bottom: 2, left: 5, right: 5},
+                    font: type === "section" ? "bold 28px" : "28px",
+                    alignment: type === "section" ? "center" : "center"
+                }).appendTo(cell);
+                if (type === "section") {
+                    cell.set("background", "#cecece");
+                }
+                cell.on("change:item", function (widget, item) {
+                    textView.set("text", item.name);
+                });
+            }
+        }).on("select", function (target, value) {
+            localStorage.setItem('city', JSON.stringify(value));
+            if (currency.page != null) {
+                currency.page.close();
+            }
+            currency.openCityCurrencies(value);
+        }).appendTo(self.selectCityPage);
     }
 };
-
 
 module.exports = new City();
---
+++ src/currency.js
@@ -0,0 +1,179 @@
+var language = require("./language");
+var registry = require("./registry");
+var moment = require('moment/min/moment-with-locales.min.js');
+moment.locale(language.getLocale());
+
+Currency = function () {
+
+    var self = this;
+    this.page = null;
+    this.view = {};
+    this.header = {};
+
+
+    this.load = function () {
+
+        console.log('currency.load');
+
+        self.view.set({
+            refreshIndicator: true,
+            refreshMessage: "loading..."
+        });
+
+        var city = JSON.parse(localStorage.getItem('city'));
+
+        fetch(registry.get('domain') + "/exchange_rate/get?city_id=" + city.id + "&best=1").then(function (response) {
+            
+            var rates = JSON.parse(response._bodyInit);
+
+            self.view.set({
+                items: rates,
+                refreshIndicator: false,
+                refreshMessage: "refreshed"
+            });
+
+            self.page.set('title', language.getString('page', 'title') + ' - ' + moment().format('LLL'));
+
+        }).catch(function (error) {
+            console.log('request failed:', error)
+        })
+    };
+
+    this.openCityCurrencies = function () {
+
+        console.log('currency.openCityCurrencies');
+
+        self.page = tabris.create("Page", {
+            title: language.getString('page', 'title'),
+            background: "#F2F2F2",
+            topLevel: true
+        });
+
+        self.header = tabris.create("Composite", {
+            layoutData: {left: 0, top: 0, right: 0},
+            background: "#BFBFBF",
+            height: 50
+        }).appendTo(self.page);
+
+        tabris.create("TextView", {
+            text: language.getString('currencyHeader', 'text'),
+            layoutData: {left: "5%", right: 0, top: 10},
+            font: "18px Roboto, sans-serif"
+        }).appendTo(self.header);
+
+        tabris.create("TextView", {
+            text: language.getString('buyHeader', 'text'),
+            layoutData: {left: "30%", right: 0, top: 10},
+            font: "18px Roboto, sans-serif"
+        }).appendTo(self.header);
+
+        tabris.create("TextView", {
+            text: language.getString('sellHeader', 'text'),
+            layoutData: {left: "55%", right: 0, top: 10},
+            font: "18px Roboto, sans-serif"
+        }).appendTo(self.header);
+
+        tabris.create("TextView", {
+            text: language.getString('nbHeader', 'text'),
+            layoutData: {left: "80%", right: 0, top: 10},
+            font: "18px Roboto, sans-serif"
+        }).appendTo(self.header);
+
+        self.view = tabris.create("CollectionView", {
+            layoutData: {
+                left: 0,
+                top: 50,
+                right: 0,
+                bottom: 0
+            },
+            itemHeight: 50,
+            refreshEnabled: true,
+            initializeCell: function (cell) {
+
+                var flagImage = tabris.create("ImageView", {
+                    layoutData: {height: 24, width: 45, left: "5%", top: 16},
+                    background: "#F2F2F2",
+                    scaleMode: 'auto'
+                }).appendTo(cell);
+
+                var currencyText = tabris.create("TextView", {
+                    layoutData: {left: [flagImage, 16], top: 11, right: 16},
+                    alignment: "left",
+                    font: "24px Roboto, sans-serif",
+                    textColor: "#000"
+                }).appendTo(cell);
+
+                var buyText = tabris.create("TextView", {
+                    layoutData: {left: "30%", top: 10, right: 10},
+                    alignment: "left",
+                    font: "24px Roboto, sans-serif",
+                    textColor: "#000"
+                }).appendTo(cell);
+
+                var sellText = tabris.create("TextView", {
+                    layoutData: {left: "55%", top: 10, right: 10},
+                    alignment: "left",
+                    font: "24px Roboto, sans-serif",
+                    textColor: "#000"
+                }).appendTo(cell);
+
+                var nbText = tabris.create("TextView", {
+                    layoutData: {left: "80%", top: 10, right: 10},
+                    alignment: "left",
+                    font: "24px Roboto, sans-serif",
+                    textColor: "#000"
+                }).appendTo(cell);
+
+                var line = tabris.create("Composite", {
+                    layoutData: {left: "2%", top: 0, right: "2%"},
+                    background: "#858585",
+                    height: 1
+                }).appendTo(cell);
+
+
+                cell.on("change:item", function (widget, item) {
+                    flagImage.set("image", {src: "./src/images/" + item.currency + ".png"});
+                    currencyText.set("text", item.currency.toUpperCase());
+                    buyText.set("text", item.buy);
+                    sellText.set("text", item.sell);
+                    nbText.set("text", item.nbrb);
+                    if (item.currency == 'usd') {
+                        line.set('height', 0);
+                    }
+                });
+            }
+        }).on("refresh", function () {
+            self.load();
+        }).on("select", function (target, value) {
+            self.createCurrencyPage(value.currency, false);
+        }).appendTo(self.page);
+
+        self.page.open();
+
+        self.load();
+    };
+
+    this.createCurrencyPage = function (currency) {
+
+        var page = tabris.create("Page", {
+            title: currency.toUpperCase(),
+            topLevel: false
+        }).open();
+
+        /*
+         fetch("http://192.168.100.2/currency/"+currency).then(function(response) {
+         console.log(response);
+         var rates = JSON.parse(response._bodyInit);
+
+
+
+         }).catch(function(error) {
+         console.log('request failed:', error)
+         })
+
+         */
+    }
+
+};
+
+module.exports = new Currency();
